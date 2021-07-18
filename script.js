12963d0e799f404ad991196e0c85a9c5b62a0e51
kjnb-0 <80792196+kjnb-0@users.noreply.github.com>
2021-07-17 18:49:48 -0700

    added api w/ placeholder values and rough display

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
-
 //https://openweathermap.org/api/one-call-api
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+//api key accfb71ce5131d183c44d819c83e21f6
 
-// USES CITY NAME! https://openweathermap.org/current
+var weatherList = document.querySelector("#weather-list");
+var fetchButton = document.querySelector("#button");
 
-// var requestURl api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+ // read userinput from textinput in html 
+  // fetch w/ userinput 
 
+function getApi() {
+  //placeholder values in requesturl - need to change to user input
+  var requestUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=47&lon=-122&exclude=hourly,minutely&appid=accfb71ce5131d183c44d819c83e21f6";
+  console.log(requestUrl);
+  //activity #4
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("fetch response");
+      console.log(data);
+      //create blank list element
 
-// Parameters
-// appid	required	Your unique API key (you can always find it on your account page under the "API key" tab)
+      //name - timezone for now, change to userinput? how work with coordinates?
+      var weatherItemName = document.createElement("li");
+      weatherItemName.textContent = data.timezone;
+      weatherList.appendChild(weatherItemName);
+      //date
+      var weatherItemDate = document.createElement("li");
+      weatherItemDate.textContent = data.current.dt;
+      weatherList.appendChild(weatherItemDate);
+      //icon for weather conditions - broken for now
+      var weatherItemIcon = document.createElement("li");
+      weatherItemIcon.textContent = data.current.weather.icon;
+      weatherList.appendChild(weatherItemIcon);
+      //current temp
+      var weatherItemTemp = document.createElement("li");
+      weatherItemTemp.textContent = data.current.temp;
+      weatherList.appendChild(weatherItemTemp);
+      //humidity
+      var weatherItemHumid = document.createElement("li");
+      weatherItemHumid.textContent = data.current.humidity;
+      weatherList.appendChild(weatherItemHumid);
+      //wind
+      var weatherItemWind = document.createElement("li");
+      weatherItemWind.textContent = data.current.wind_speed;
+      weatherList.appendChild(weatherItemWind);
+      //uv index
+      var weatherItemUV = document.createElement("li");
+      weatherItemUV.textContent = data.current.uvi;
+      weatherList.appendChild(weatherItemUV);
+    });
+}
 
-
-// Browser Fetch Method
-// TODO: Comment on how Fetch returns an API call
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('Fetch Response \n-------------');
-    console.log(data);
-  });
-
-  // read userinput from textinput in html 
-  // fetch w/ userinput
-  // return data
-  //push that data into .cardtext p 
+fetchButton.addEventListener("click", getApi);
